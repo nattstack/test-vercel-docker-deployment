cec22f22e5353638b31a5e56bdf49bef7980fec1
Natt <natt.lefevre@gmail.com>
2026-07-22 00:35:58 -0700

    fix: add name attributes to form inputs and set button type for login form

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,8 @@ export const Route = createFileRoute("/")({
     function onSubmit(event: React.SubmitEvent<HTMLFormElement>): void {
       event.preventDefault()
 
+      console.log(event.currentTarget)
+
       const formData = new FormData(event.currentTarget)
       const email = formData.get("email") as string
       const password = formData.get("password") as string
@@ -20,15 +22,15 @@ export const Route = createFileRoute("/")({
         <form className="flex w-full max-w-320 flex-col gap-4" onSubmit={onSubmit}>
           <Label htmlFor="email">Email</Label>
           <Spacer height={4} />
-          <Input defaultValue="larry@example.com" id="email" type="email" />
+          <Input defaultValue="larry@example.com" id="email" name="email" type="email" />
           <Spacer height={16} />
 
           <Label htmlFor="password">Password</Label>
           <Spacer height={4} />
-          <Input defaultValue="password" id="password" type="text" />
+          <Input defaultValue="password" id="password" name="password" type="text" />
 
           <Spacer height={24} />
-          <Button isFullWidth label="Login" size={48} />
+          <Button isFullWidth label="Login" size={48} type="submit" />
         </form>
       </Column>
     )
