32945e7b9e3088a22a59dcc6a9bb06d1c356dc87
Natt <natt.lefevre@gmail.com>
2026-07-22 00:27:36 -0700

    feat: add login form to main route component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,23 @@
-import { Column } from "@nattstack/ui"
+import { Button, Column, Input, Label, Spacer } from "@nattstack/ui"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/")({
   component: function RouteComponent() {
     return (
       <Column className="h-dvh items-center justify-center">
-        Testing vercel docker deployment
+        <Column className="w-full max-w-320">
+          <Label htmlFor="email">Email</Label>
+          <Spacer height={4} />
+          <Input id="email" type="email" value="larry@example.com" />
+          <Spacer height={16} />
+
+          <Label htmlFor="password">Password</Label>
+          <Spacer height={4} />
+          <Input id="password" type="text" value="password" />
+
+          <Spacer height={24} />
+          <Button isFullWidth label="Login" size={48} />
+        </Column>
       </Column>
     )
   },
